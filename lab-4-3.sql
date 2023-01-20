bf20e590b574d75909ec0b7936d5d104327adbf2
aac3006 <alicia.alessandri@kellogg.northwestern.edu>
2023-01-20 21:55:31 +0000

    lab4

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,12 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+SELECT players.first_name, players.last_name, teams.name, teams.year, stats.home_runs
+FROM stats
+INNER JOIN teams ON stats.team_id = teams.id
+INNER JOIN players ON stats.player_id = players.id
+WHERE teams.year = 2019
+ORDER BY stats.home_runs DESC LIMIT 1;
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
